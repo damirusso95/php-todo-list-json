94636cd1e6c082537cd53c1deb67c7954ac17eca
damirusso95 <damirusso95@gmail.com>
2024-06-15 16:45:26 +0200

    input addtask

--- api-client/main.js
+++ api-client/main.js
@@ -3,20 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            list: []
+            list: [],
+            newTask: ""
         }
     },
-
     methods: {
-
+        addTask() {
+            console.log("aggiungi task", this.newTask);
+        }
     },
     mounted() {
         console.log('recupero dati dal server');
-
         axios.get("../Api.php").then(results => {
-            console.log("risultati:" , results);
+            console.log("risultati:", results);
             this.list = results.data;
-
         });
     }
 }).mount('#app')
